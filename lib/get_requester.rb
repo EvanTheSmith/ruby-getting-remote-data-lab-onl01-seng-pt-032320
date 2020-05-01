71f2a1dee72115f5bf76c9a8a3c7aec406723089
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-05-01 00:19:03 -0500

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
-# Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+require 'pry'
+
+class GetRequester
+    attr_accessor :url
+
+    def initialize(string_url)
+        @url = string_url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+        
+    end
+
+    def parse_json
+        JSON.parse(get_response_body)
+    end
+
+end
